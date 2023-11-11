8c47d72e3949ca872e5ad61bd9af371f9b46aab6
Edgar Rezende <edgar.rezende@mercadolivre.com>
2023-11-10 21:30:16 -0300

    updates

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require("path");
+const { VueLoaderPlugin } = require('vue-loader');
+const webpack = require('webpack')
 
 const mode = process.env.NODE_ENV === "production" ? "production" : "development";
 const devtool = process.env.NODE_ENV === "production" ? undefined : "source-map";
@@ -17,11 +19,27 @@ const mainConfig = {
                 test: /\.tsx?$/,
                 loader: "ts-loader",
             },
+
+
+            {
+                test: /\.vue$/, // Add this rule for .vue files
+                loader: "vue-loader",
+            },
+
         ],
     },
     resolve: {
-        extensions: [".ts", ".js"],
+        // alias: {
+        //     vue$: "vue/dist/vue.esm.js",
+        // },
+        extensions: ['.ts', '.js', '.vue'],
+        // extensions: [".ts", ".js"],
     },
+    plugins: [
+        // ...other plugins...
+        new VueLoaderPlugin()
+    ],
+
     mode,
     target: "electron-main",
     node: false,
@@ -30,6 +48,7 @@ const mainConfig = {
         sqlite3: "commonjs sqlite3",
     },
 };
+
 
 const rendererConfig = {
     entry: path.join(__dirname, "src", "renderer", "renderer.ts"),
@@ -43,14 +62,34 @@ const rendererConfig = {
                 test: /\.tsx?$/,
                 loader: "ts-loader",
             },
+
+            {
+                test: /\.vue$/, // Add this rule for .vue files
+                loader: "vue-loader",
+            },
+
+            {
+                test: /\.(png|jpg|gif|svg)$/,
+                loader: 'file-loader',
+            },
+
         ],
     },
     resolve: {
-        alias: {
-            vue$: "vue/dist/vue.esm.js",
-        },
-        extensions: [".ts", ".js"],
+        // alias: {
+        //     vue$: "vue/dist/vue.esm.js",
+        // },
+        extensions: ['.ts', '.js', '.vue'],
     },
+    plugins: [
+        // ...other plugins...
+        new VueLoaderPlugin(),
+        new webpack.DefinePlugin({
+            __VUE_OPTIONS_API__: false,
+            __VUE_PROD_DEVTOOLS__: true,
+        }),        
+    ],
+
     mode,
     target: "electron-renderer",
     node: false,
